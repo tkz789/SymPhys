72186d2d15bcf0acf8aea863275d19227b2ae3dd
Dawid Zapolski <dawid.zapolski@student.uj.edu.pl>
2024-05-10 22:40:57 +0200

    Added SimplePendulum

---
+++ SimplePendulum.java
@@ -0,0 +1,31 @@
+import java.awt.geom.Point2D;
+import static java.lang.Math.*;
+
+
+/**
+ * Mathematical pendulum with small angle approximation
+ */
+public class SimplePendulum {
+
+    double length, gravity, initial_angle = 0.1, initial_velocity = 0;
+    double omega;
+    SimplePendulum(double length, double gravity){
+        this.gravity = gravity;
+        this.length = length;
+        omega = sqrt(gravity/length);
+    }
+    SimplePendulum(double length){
+        this(length, 9.81);
+    }
+
+    public double angle(double time){
+        return initial_angle * cos(omega * time) + initial_velocity  * sin(omega * time);
+    }
+
+    public Point2D.Double position(double time){
+        double theta = angle(time);
+        double x = length * sin(theta);
+        double y = -length * cos(theta);
+        return new Point2D.Double(x, y);
+    }
+}
