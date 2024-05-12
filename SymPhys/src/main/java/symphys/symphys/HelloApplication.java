6d034ca71af98098f475b9b6574600886d90471f
torelq <torelq@gmail.com>
2024-05-13 00:38:37 +0200

    cokolwiek ćwierćdziałającego

--- SymPhys/src/main/java/symphys/symphys/HelloApplication.java
+++ SymPhys/src/main/java/symphys/symphys/HelloApplication.java
@@ -12,6 +12,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        HelloController controller = fxmlLoader.getController();
+        controller.stage = stage;
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
