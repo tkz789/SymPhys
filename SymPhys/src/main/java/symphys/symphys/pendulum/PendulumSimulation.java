f90912b68c896884bafad79ae2c2f171a45306f8
strefan43 <str3fan@gmail.com>
2024-05-12 22:33:47 +0200

    Pendulum with simple parameters

--- SymPhys/src/main/java/symphys/symphys/pendulum/PendulumSimulation.java
+++ SymPhys/src/main/java/symphys/symphys/pendulum/PendulumSimulation.java
@@ -1,16 +1,68 @@
 package symphys.symphys.pendulum;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PendulumSimulation {
     public static void startSimulation(Stage stage){
-        PendulumAnimation animation = new PendulumAnimation(new SimplePendulum(200, 9.81));
-        Group root = new Group(animation.rod, animation.bob);
+        Slider lengthSlider=new Slider(50, 275, 175);
+        Slider gravitySlider=new Slider(0, 100, 9.81);
+        Slider initialAngleSlider=new Slider(0, 2*Math.PI, 1);
+        Label lengthLabel=new Label("Rod length: 175,00");
+        Label gravityLabel=new Label("Gravity: 9,81");
+        Label initialAngleLabel=new Label("Initial angle: 1,00");
+        VBox sliderBox=new VBox(10);
+
+        lengthSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+
+                    public void changed(ObservableValue<? extends Number > observable, Number oldValue, Number newValue)
+                    {
+                        lengthLabel.setText("Rod length: " + String.format("%.2f", newValue.doubleValue()));
+                    }
+                });
+        gravitySlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+
+                    public void changed(ObservableValue<? extends Number > observable, Number oldValue, Number newValue)
+                    {
+                        gravityLabel.setText("Gravity: " + String.format("%.2f", newValue.doubleValue()));
+                    }
+                });
+        initialAngleSlider.valueProperty().addListener(
+                new ChangeListener<Number>() {
+
+                    public void changed(ObservableValue<? extends Number > observable, Number oldValue, Number newValue)
+                    {
+                        initialAngleLabel.setText("Initial angle: " + String.format("%.2f", newValue.doubleValue()));
+                    }
+                });
+
+        EventHandler<ActionEvent> startButtonHandler=(e) -> {
+                PendulumAnimation animation = new PendulumAnimation(new SimplePendulum(lengthSlider.getValue(), gravitySlider.getValue(), initialAngleSlider.getValue()));
+                Group animationRoot = new Group(animation.rod, animation.bob);
+                animation.start();
+                Scene animationScene=new Scene(animationRoot, 500, 300);
+                stage.setScene(animationScene);
+                stage.show();
+        };
+        Button startButton=new Button("Start animation");
+        startButton.setOnAction(startButtonHandler);
+
+        sliderBox.getChildren().addAll(lengthSlider, lengthLabel, gravitySlider, gravityLabel, initialAngleSlider, initialAngleLabel, startButton);
+
+        Group root = new Group(sliderBox);
         Scene scene = new Scene(root, 500, 300);
         stage.setScene(scene);
         stage.show();
-        animation.start();
     }
 }
--- SymPhys/src/main/java/symphys/symphys/pendulum/SimplePendulum.java
+++ SymPhys/src/main/java/symphys/symphys/pendulum/SimplePendulum.java
@@ -9,15 +9,16 @@ import static java.lang.Math.*;
  */
 public class SimplePendulum extends Pendulum{
 
-    double length, gravity, initial_angle = 1, initial_velocity = 0;
+    double length, gravity, initial_angle, initial_velocity = 0;
     double omega;
-    SimplePendulum(double length, double gravity){
+    SimplePendulum(double length, double gravity, double initial_angle){
         this.gravity = gravity;
         this.length = length;
+        this.initial_angle = initial_angle;
         omega = sqrt(gravity/length);
     }
     SimplePendulum(double length){
-        this(length, 9.81);
+        this(length, 9.81, 0.1);
     }
 
     public double angle(double time){
