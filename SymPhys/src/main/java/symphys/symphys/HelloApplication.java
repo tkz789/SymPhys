f6831229f1782d316a2998cc2cf22790272d3a3f
strefan43 <str3fan@gmail.com>
2024-05-13 17:59:17 +0200

    Some choice interface

--- SymPhys/src/main/java/symphys/symphys/HelloApplication.java
+++ SymPhys/src/main/java/symphys/symphys/HelloApplication.java
@@ -1,11 +1,20 @@
 package symphys.symphys;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import symphys.symphys.Doppler.Animate;
+//import symphys.symphys.Doppler.Animate;
 import symphys.symphys.fields.Main;
+import symphys.symphys.pendulum.PendulumAnimation;
 import symphys.symphys.pendulum.PendulumSimulation;
 import symphys.symphys.GivenForce.AnimationGivenForce;
+import symphys.symphys.pendulum.SimplePendulum;
 
 import java.io.IOException;
 
@@ -22,9 +31,26 @@ public class HelloApplication extends Application {
 //        stage.setScene(scene);
 //        stage.show();
         stage.setTitle("SymPhys");
+        Label simOptionsLabel = new Label("Choose the simulation:");
+        Button pendulumButton = new Button("Pendulum simulation");
+        Button electricFieldButton = new Button("Electric field simulation");
+        Button givenForceButton = new Button("Given force simulation");
+        VBox optionsBox = new VBox(10, simOptionsLabel  , pendulumButton, electricFieldButton, givenForceButton);
+        pendulumButton.setOnAction((e) ->
+            PendulumSimulation.startSimulation(stage)
+        );
+        electricFieldButton.setOnAction((e) ->
+            Main.start(stage)
+        );
+        givenForceButton.setOnAction((e) ->
+            AnimationGivenForce.start(stage)
+        );
+        Scene scene = new Scene(optionsBox, 500, 300);
+        stage.setScene(scene);
+        stage.show();
 //        Animate.startSym1(stage);
 //        AnimationGivenForce.start(stage);
 //        Main.start(stage);
-        PendulumSimulation.startSimulation(stage);
+//        PendulumSimulation.startSimulation(stage);
     }
 }
