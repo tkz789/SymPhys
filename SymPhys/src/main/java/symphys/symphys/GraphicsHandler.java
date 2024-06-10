c6196f4c51b6f1b1c9e506a277edfcafd8b29ff2
Dawid Zapolski <dawid.zapolski@student.uj.edu.pl>
2024-06-10 02:12:48 +0200

    MVVM GraphicsHandler

--- SymPhys/src/main/java/symphys/symphys/GraphicsHandler.java
+++ SymPhys/src/main/java/symphys/symphys/GraphicsHandler.java
@@ -1,5 +1,10 @@
 package symphys.symphys;
 
-public class GraphicsHandler {
+import javafx.scene.Node;
 
+import java.util.List;
+
+public interface GraphicsHandler<T extends SimulationModel> {
+    public List<Node> getObjects();
+    public void drawSimulation(T model);
 }
